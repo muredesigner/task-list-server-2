8f02c4df32f2005cdf50e820359969da34093135
Marycarmen Mora <muredesigner@gmail.com>
2023-11-21 00:05:04 -0400

    proyecto cuatro de express

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
 const port = 3000;
 
@@ -7,10 +8,31 @@ const listEditRouter = require('./list-edit-router');
 
 app.use(express.json());
 
+let users = [
+    {
+        "username": "user1",
+        "password": "password1"
+    },
+];
+
+app.post('/login', (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = users.find(u => u.username === username && u.password === password);
+
+    if (user) {
+        const accessToken = jwt.sign({ username: user.username }, process.env.ACCESS_TOKEN_SECRET);
+        res.json({ accessToken });
+    } else {
+        res.status(401).send('Username or password incorrect');
+    }
+});
+
 app.use((req, res, next) => {
     const validMethods = ['GET', 'POST', 'PUT', 'DELETE'];
     if (!validMethods.includes(req.method)) {
-        return res.status(404).send('Not found');
+        return res.status(405).send('Method Not Allowed');
     }
     next();
 });
@@ -18,7 +40,36 @@ app.use((req, res, next) => {
 app.use('/', listViewRouter);
 app.use('/', listEditRouter);
 
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+}
+
+app.get('/protected', authenticateToken, (req, res) => {
+  res.send('Welcome to the protected route!');
+});
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+      if (err) return res.sendStatus(403);
+      req.user = user;
+      next();
+  });
+}
+
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
-
